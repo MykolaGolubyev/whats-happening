ea64a2d5354bb804b1eb4c56c27188e7b3480325
Christian Tejada <Christian@Bot.local>
2017-09-08 12:39:45 -0400

    Scrollbar addition

---
+++ products/web/src/scrollbar/Scrollbar.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './Scrollbar.css';
+
+interface Props {
+    date: string;
+}
+
+const Scrollbar = ({date}: Props) => {
+    return (
+        <div className="Container"></div>
+    );
+};
+
+export {Scrollbar};
