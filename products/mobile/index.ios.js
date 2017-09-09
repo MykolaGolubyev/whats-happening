9444b1ff9657c4848a2964966cdf766ace03efc3
Tim Chen <chengt@gmail.com>
2017-09-09 10:00:17 -0400

    prepare to chagne status bar style

--- products/mobile/index.ios.js
+++ products/mobile/index.ios.js
@@ -1,9 +1,13 @@
 import React, {Component} from 'react';
-import {AppRegistry} from 'react-native';
+import {AppRegistry, StatusBar} from 'react-native';
 
 import HomeScreen from './src/screens/WHScreen';
 
 export default class WhatsHappeningMobile extends Component {
+    componentDidMount() {
+        StatusBar.setBarStyle('dark-content', true);
+    }
+
     render() {
         return <HomeScreen/>;
     }
