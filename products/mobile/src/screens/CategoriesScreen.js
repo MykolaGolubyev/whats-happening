feb70b6f600dabcb1a77168fbdbd89c70e518581
Tim Chen <chengt@gmail.com>
2017-09-09 18:22:37 -0400

    playing around with view

--- products/mobile/src/screens/CategoriesScreen.js
+++ products/mobile/src/screens/CategoriesScreen.js
@@ -1,17 +1,15 @@
 import React, {PureComponent} from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, ScrollView} from 'react-native';
+import {Card} from 'react-native-elements';
 import styles from '../styles/WHStyle';
 
 const CategoryCard = (props) => {
     const {name, description, subcount} = props.category;
     return (
-        <View style={[styles.categoryCard]}>
-            <View style={styles.categoryDescription}>
-                <Text style={styles.cname}>{name}</Text>
-                <Text style={styles.subcount}>({subcount})</Text>
-            </View>
+        <Card title={name} containerStyle={styles.categoryCard} titleStyle={styles.cname}>
             <Text style={styles.description}>{description}</Text>
-        </View>
+            <Text style={styles.subcount}>{`${subcount} members`}</Text>
+        </Card>
     )
 };
 
@@ -19,11 +17,11 @@ export default class CategoriesScreen extends PureComponent {
     render() {
         let categoryCards = this.props.categories.map(c => <CategoryCard key={c.name} category={c}/>);
         return (
-            <View style={styles.categoriesContainer}>
+            <ScrollView style={[styles.categoriesContainer]}>
                 <View style={styles.categoriesRow}>
                     {categoryCards}
                 </View>
-            </View>
+            </ScrollView>
         );
     }
 }
--- products/mobile/src/styles/WHStyle.js
+++ products/mobile/src/styles/WHStyle.js
@@ -1,6 +1,6 @@
 import {Dimensions, StyleSheet} from "react-native";
 
-export default styles = StyleSheet.create({
+export default WHStyle = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#272a2c',
@@ -24,8 +24,7 @@ export default styles = StyleSheet.create({
         borderColor: '#c0c0c0',
         borderWidth: 1,
         height: 100,
-        width: 150,
-        padding: 2
+        width: Dimensions.get('window').width / 3
     },
     categoryDescription: {
         flex: 1,
@@ -40,9 +39,9 @@ export default styles = StyleSheet.create({
         paddingLeft: 20,
         paddingRight: 20,
     },
-    cname: {flex: 1, fontSize: 15, fontWeight: 'bold', fontStyle: 'italic', color: '#676767'},
-    description: {flex: 2, textAlign: 'left', fontSize: 10, fontStyle: 'italic', color: '#dfdfdf'},
-    subcount: {flex: 1, textAlign: 'right', fontSize: 10, fontStyle: 'italic', color: '#bbbb2f'},
+    cname: {fontSize: 15, fontWeight: 'bold', fontStyle: 'italic', color: '#676767'},
+    description: {textAlign: 'left', fontSize: 10, fontStyle: 'italic', color: '#8f95af'},
+    subcount: {textAlign: 'right', fontSize: 10, fontStyle: 'italic', color: '#bbbb2f'},
     actionIcon: {fontSize: 26, height: 26, color: '#ccc'},
     tabBar: {backgroundColor: '#000'}
 });
